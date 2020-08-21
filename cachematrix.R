0dda801c5b48bb780377c382af3d85d78976f0da
EliCohen2 <69922410+EliCohen2@users.noreply.github.com>
2020-08-20 21:20:33 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Set input x as matrix, value of "t" as the null and changed references to "solve"
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  t <- NULL
+  set <- function(y) {
+    x <<- y
+    t <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) t <<- solve
+  getsolve <- function() t
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## Changed "m" to "t" and set references to "solve"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        t <- x$getsolve()
+        if(!is.null(t)) {
+          message("getting inversed matrix")
+          return(t)
+        }
+        data <- x$get()
+        t <- solve(data,...)
+        x$setsolve(t)
+        t
 }
+
